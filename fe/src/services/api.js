99b183bad339aea4944edf2e967662e0ab17a94e
Vitalii Rybachuk <vitalii.rybachuk@infopulse.com>
2023-12-27 19:26:38 +0200

    update

--- fe/src/services/api.js
+++ fe/src/services/api.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export const api = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
-  // baseURL: 'http://localhost:4000',
+  //  baseURL: 'http://localhost:4000',
   timeout: 12000
 });
